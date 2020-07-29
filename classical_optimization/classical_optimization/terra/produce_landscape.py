660b5d3ab661b16657583526bdc85ad6374872fc
Anthony Polloreno <ampolloreno@gmail.com>
2020-07-28 21:06:06 -0600

    now using google code

--- classical_optimization/classical_optimization/terra/produce_landscape.py
+++ classical_optimization/classical_optimization/terra/produce_landscape.py
@@ -7,12 +7,14 @@ import numpy as np
 from qiskit import Aer, execute
 # I may have modified my qiskit - this adds on an attribute when I import.
 from qiskit.providers.aer.extensions import snapshot_density_matrix
+from recirq.qaoa.simulation import exact_qaoa_values_on_grid
 import sys
 import time
 
-max_gamma = 2*np.pi
-max_beta = np.pi
+max_gamma = np.pi/2
+max_beta = np.pi/2
 discretization = int(sys.argv[2])
+discretization = 20
 gammas, betas = produce_gammas_betas(discretization, max_gamma, max_beta)
 
 filename = sys.argv[1]
@@ -21,18 +23,19 @@ if landscape_string not in read_graph(filename).keys():
     graph = read_graph(filename)['graph']
     num_qubits = len(graph.nodes)
     start = time.time()
-    simulator = Aer.get_backend('statevector_simulator')
-    experiments = []
-    for gamma in gammas:
-        for beta in betas:
-            circuit = maxcut_qaoa_circuit(gammas=[gamma], betas=[beta], p=1, num_qubits=num_qubits, weights=weights(graph), measure=False)
-            experiments.append(circuit)
-    job = execute(experiments, backend=simulator)
-    expectations = [np.real(cost(job.result().get_statevector(experiment), num_qubits=num_qubits, weights=weights(graph))) for experiment in experiments]
-
-    landscape = np.zeros((2*discretization, discretization))
-    for i, gamma in enumerate(gammas):
-        for j, beta in enumerate(betas):
-            landscape[i][j] = expectations[i*len(betas) + j]
+    # simulator = Aer.get_backend('statevector_simulator')
+    # experiments = []
+    # for gamma in gammas:
+    #     for beta in betas:
+    #         circuit = maxcut_qaoa_circuit(gammas=[gamma], betas=[beta], p=1, num_qubits=num_qubits, weights=weights(graph), measure=False)
+    #         experiments.append(circuit)
+    # job = execute(experiments, backend=simulator)
+    # expectations = [np.real(cost(job.result().get_statevector(experiment), num_qubits=num_qubits, weights=weights(graph))) for experiment in experiments]
+    #
+    # landscape = np.zeros((2*discretization, discretization))
+    # for i, gamma in enumerate(gammas):
+    #     for j, beta in enumerate(betas):
+    #         landscape[i][j] = expectations[i*len(betas) + j]
+    landscape = exact_qaoa_values_on_grid(graph)
     stop = time.time()
     write_graph(graph, {landscape_string: landscape, landscape_string + '_time': stop-start})
