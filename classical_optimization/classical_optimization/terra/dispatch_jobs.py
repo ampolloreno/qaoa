cc007faa3b9a115a61dccadd6b337688745fe4e3
Anthony Polloreno <ampolloreno@gmail.com>
2020-08-03 17:32:25 -0600

    small boys

--- classical_optimization/classical_optimization/terra/dispatch_jobs.py
+++ classical_optimization/classical_optimization/terra/dispatch_jobs.py
@@ -78,7 +78,7 @@ seed = 137
 np.random.seed(seed)
 # We might want more graphs, additionally we want to make sure that the graphs are saved in a way that it is easy
 # to recover the data, and make sure that new graphs are non-isomorphic.
-num_graphs_gen = 10
+num_graphs_gen = 20
 graphs = []
 # We might want larger d.
 d = 5
@@ -86,7 +86,7 @@ p = .2
 # We will scan over this parameter, it's not clear if it should scale multiplicatively, or additively.
 discretization = 10
 
-for num_qubits in [12, 16, 20, 24]:
+for num_qubits in [4, 8, 12, 16, 20, 24]:
     graohs = []
     for _ in range(num_graphs_gen):
         g = nx.generators.classic.complete_graph(num_qubits)
