dd3c29f46e1de5502a538d930fce7fa924ee8928
Anthony Polloreno <ampolloreno@gmail.com>
2020-08-03 16:01:30 -0600

    moar graphs

--- classical_optimization/classical_optimization/terra/dispatch_jobs.py
+++ classical_optimization/classical_optimization/terra/dispatch_jobs.py
@@ -78,7 +78,7 @@ seed = 137
 np.random.seed(seed)
 # We might want more graphs, additionally we want to make sure that the graphs are saved in a way that it is easy
 # to recover the data, and make sure that new graphs are non-isomorphic.
-num_graphs_gen = 1
+num_graphs_gen = 10
 graphs = []
 # We might want larger d.
 d = 3
@@ -86,7 +86,7 @@ p = .2
 # We will scan over this parameter, it's not clear if it should scale multiplicatively, or additively.
 discretization = 10
 
-for num_qubits in [4, 8, 12, 16, 20, 24, 28, 32, 36, 40]:
+for num_qubits in [4, 8, 12, 16, 20, 24, 28, 30]:
     graohs = []
     for _ in range(num_graphs_gen):
         graphs.append(nx.generators.random_graphs.watts_strogatz_graph(num_qubits, d, p, seed=seed))
